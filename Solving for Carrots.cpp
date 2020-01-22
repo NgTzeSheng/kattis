cb2820b391e0feeea9cd81c829c66e78e567b03f
Ng Tze Sheng <55088883+NgTzeSheng@users.noreply.github.com>
2020-01-22 12:25:42 +0800

    Solving for Carrots

---
+++ Solving for Carrots.cpp
@@ -0,0 +1,12 @@
+// The question seems long and complicated, but essentially, you just need to return the 2nd input. (trick question sort of)
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << b;
+}
